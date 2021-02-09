85d0868165bc29a8c7b0e39c293691a0a7e163f4
saraezzooff <58671265+saraezzooff@users.noreply.github.com>
2021-02-09 22:01:18 +0200

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,13 @@
+window.onload = function(){ document.getElementById("loader").style.display = "none" }
+
+
+
+// document.onreadystatechange = function () {
+//     if (document.readyState === 'complete') {
+//       //Remove loader class from div
+
+//       setTimeout(function(){ 
+//         $('#loader').css('display', 'none');
+//     }, 5000);
+//     }
+//   }
